8a55f0ea4b0e30c7ecd8e1cd824e7122f3a8f21c
diyorbek <diyorbekabdulaxatov40@gmail.com>
2025-12-01 12:07:48 +0500

    basic templ

--- server/server.go
+++ server/server.go
@@ -1,14 +1,1 @@
-package server
-
-import (
-	"context"
-	hellov1 "github.com/diyor200/learn-grpc/proto"
-)
-
-type Server struct {
-	conn hellov1.UnimplementedHelloServer
-}
-
-func (s *Server) SayHello(ctx context.Context, in *hellov1.HelloRequest) (*hellov1.HelloResponse, error) {
-	return &hellov1.HelloResponse{HelloText: "Hello " + in.Name}, nil
-}
+package main
